472761e51018f3956497dd25906f8bfe393ceab1
Nicolas Durand <nicod@happinov.fr>
2018-04-26 10:19:48 +0200

    Completed touch test to display click, touchstart, touchend and touchcancel events.

--- www/js/main.js
+++ www/js/main.js
@@ -1,16 +1,51 @@
 $(document).ready(function(){
+	$(function() {
+		FastClick.attach(document.body);
+	});
+
 	$('.testButton').on('click', function(event){
 		elem = document.createElement('div');
 		$(elem).addClass('clickElement').html('CLICK');
 		$('.resultClick').append(elem);
 		setTimeout(function() {$('.resultClick').children().last().remove();}, 2000);
 	});
-	$('.testButton').on('touchend', function(event){
+	// $('.testButton').on('touchend', function(event){
+	// 	elem = document.createElement('div');
+	// 	$(elem).addClass('touchElement').html('TOUCHEND');
+	// 	$('.resultTouchend').append(elem);
+	// 	setTimeout(function() {$('.resultTouchend').children().last().remove();}, 2000);
+	// });
+	$('.testButton').on('touchstart', function(event){
 		elem = document.createElement('div');
-		$(elem).addClass('touchendElement').html('TOUCHEND');
-		$('.resultTouchend').append(elem);
-		setTimeout(function() {$('.resultTouchend').children().last().remove();}, 2000);
-	});
+		$(elem).addClass('touchElement').html('TOUCHSTART');
+		$('.resultTouchstart').append(elem);
+		setTimeout(function() {$('.resultTouchstart').children().last().remove();}, 2000);
 
-	// function setInput(numValue){};
+		var eventCancelled = new Boolean(false);
+		target=event.target;
+		console.log('Touchstart event',event,' on target:',target);
+		$(target).one('touchcancel', function(event){
+			eventCancelled = true;
+
+			elem = document.createElement('div');
+			$(elem).addClass('touchElement').html('TOUCHCANCEL');
+			$('.resultTouchcancel').append(elem);
+			setTimeout(function() {$('.resultTouchcancel').children().last().remove();}, 2000);
+		});
+		$(target).one('touchend', function(event){
+			if (eventCancelled == false) {
+				elem = document.createElement('div');
+				$(elem).addClass('touchElement').html('TOUCHEND');
+				$('.resultTouchend').append(elem);
+				setTimeout(function() {$('.resultTouchend').children().last().remove();}, 2000);
+			}
+			$(target).off('touchcancel');
+		});
+	});
+	$('.testButton').on('touchcancel', function(event){
+		elem = document.createElement('div');
+		$(elem).addClass('touchElement').html('TOUCHCANCEL');
+		$('.resultTouchcancel').append(elem);
+		setTimeout(function() {$('.resultTouchcancel').children().last().remove();}, 2000);
+	});
 });
